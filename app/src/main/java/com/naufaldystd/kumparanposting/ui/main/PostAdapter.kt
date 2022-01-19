8998fd97a5b5841862a2e28644a61fbfa03a5bea
blitzkz23 <blitzaldykazu@gmail.com>
2022-01-19 13:31:29 +0700

    Layout adjustment for posts list

--- app/src/main/java/com/naufaldystd/kumparanposting/ui/main/PostAdapter.kt
+++ app/src/main/java/com/naufaldystd/kumparanposting/ui/main/PostAdapter.kt
@@ -1,13 +1,16 @@
 package com.naufaldystd.kumparanposting.ui.main
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.naufaldystd.kumparanposting.R
 import com.naufaldystd.kumparanposting.data.source.remote.response.PostResponseItem
 import com.naufaldystd.kumparanposting.databinding.ItemPostBinding
 
 class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
 	private var listPost = ArrayList<PostResponseItem>()
+	private lateinit var context: Context
 
 	fun setPost(posts: List<PostResponseItem>?) {
 		if (posts == null) return
@@ -19,8 +22,13 @@ class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
 		fun bind(post: PostResponseItem) {
 			with(binding) {
 				postTitle.text = post.title
-				postUser.text = post.userId.toString()
+				val userId = post.userId.toString()
+				postUser.text = itemView.context.getString(R.string.by_user, userId)
 				postMessage.text = post.body
+
+				itemView.setOnClickListener {
+
+				}
 			}
 		}
 	}
@@ -31,6 +39,7 @@ class PostAdapter : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
 	}
 
 	override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
+		context = holder.itemView.context
 		val post = listPost[position]
 		holder.bind(post)
 	}
